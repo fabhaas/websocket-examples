6f7ab66fec520e54b19a8696d4db82ba1af7d1fd
fahaas <fahaas@localhost.localdomain>
2019-01-13 11:38:20 +0100

    implemented chat

--- client/client.js
+++ client/client.js
@@ -1,12 +1,47 @@
 // create WebSocket connection.
 const socket = new WebSocket("ws://localhost:8080", "chat");
 
+function appendMsg(msg, user) {
+    document.getElementById("lbMsg").innerHTML += `<li>${user}: ${msg}</li>`;
+}
+
+function sendMsg(msg) {
+    socket.send(JSON.stringify({ "type": "msg", "data": msg}));
+}
+
+function sendMsgEvent(e) {
+    const txtMsg = document.getElementById("txtMsg");
+    const msg = txtMsg.value;
+    
+    appendMsg(msg, "You");
+    txtMsg.value = "";
+    sendMsg(msg);
+}
+
 // connection opened
-socket.addEventListener("open", function (event) {
-    socket.send("Hello Server!");
+socket.addEventListener("open", function (e) {
+    console.log("connection opened");
 });
 
+
 // listen for messages
-socket.addEventListener("message", function (event) {
-    console.log("Message from server: %s", event.data);
+socket.addEventListener("message", function (e) {
+    const msg = JSON.parse(e.data);
+    switch (msg.type) {
+        case "msg":
+            appendMsg(msg.data, msg.sender);
+            break;
+        case "sendID":
+            document.getElementById("lUser").innerHTML = "User: " + msg.data;
+            break;
+        default:
+            console.error("Unknown type");
+            break;
+    }
+});
+
+document.getElementById("btnSend").addEventListener("click", (e) => sendMsgEvent(e));
+document.getElementById("txtMsg").addEventListener("keyup", (e) => {
+    if (e.keyCode === 13)
+        sendMsgEvent(e);
 });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+//for generating id
+const crypto = require("crypto");
+
 //simple server to access index.html
 const express = require("express");
 const app = express();
@@ -9,17 +12,34 @@ app.listen(3000);
 const WebSocket = require("ws");
 
 //create server on port 8080
-const wss = new WebSocket.Server({ 
+const wss = new WebSocket.Server({
   port: 8080
 });
 
 //event handling
 wss.on("connection", function connection(ws) {
+  //generate id
+  let id = crypto.randomBytes(8).toString("hex");
+
   //send message when connection is established
-  ws.send("connection established");
+  ws.send(JSON.stringify({ "type": "sendID", "data": id }));
 
   //listen for messages
-  ws.on("message", function incoming(message) {
-    console.log("received: %s", message);
+  ws.on("message", function incoming(data) {
+    const msg = JSON.parse(data);
+
+    switch (msg.type) {
+      case "msg":
+        wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN)
+            client.send(JSON.stringify({ "type": "msg", "sender": id, "data": msg.data }));
+        });
+        console.log("received: %s from %s", msg.data, id);
+        break;
+
+      default:
+        console.error("Unknown type");
+        break;
+    }
   });
 });
